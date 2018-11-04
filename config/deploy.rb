a3971649a53c58d2ec2aea03786cfdeaff7bd17d
kuldeep-surendra <ksurendra@qwinix.io>
2018-11-04 22:27:42 +0530

    adds nginx restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,6 +58,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
+      run "sudo service nginx restart"
       invoke 'puma:restart'
     end
   end
